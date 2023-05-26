f2351c6039c64d4d7ca37bbca020689045d2902d
piroyoung <piroyoung@users.noreply.github.com>
2023-05-26 11:12:40 +0900

    add: topword

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/anaregdesign/custom-skill-builder/service"
+	"github.com/anaregdesign/custom-skill-builder/skill"
+	"github.com/anaregdesign/custom-skill-example/process"
+)
+
+func main() {
+	top10Skill := skill.NewSkill("content", "topWords", func(s string) ([]string, error) {
+		return process.GetTopWord(s, 10)
+	})
+	book := skill.NewBook()
+	book.Register("top10", top10Skill.Flatten())
+
+	svc := service.NewCustomSkillService(book)
+	svc.Run()
+
+}
